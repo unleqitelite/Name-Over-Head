b49b2e1f131e90043cb8bd521a535f6b6e341c04
unleqitelite <45175738+unleqitelite@users.noreply.github.com>
2020-11-04 15:23:58 +0100

    Create RendererLivingEntity.java

---
+++ RendererLivingEntity.java
@@ -0,0 +1,61 @@
+//
+//NOTE: Add this method to your RendererLivingEntity.java class.
+//
+
+public void renderNameTag(T entity, double x, double y, double z)
+    {
+        if (entity.getName().equals(Minecraft.getMinecraft().getSession().getUsername()) && SettingsManager.loadEnabledFromFile("ModIngameName") == true && entity instanceof AbstractClientPlayer) {
+        {
+                double d0 = entity.getDistanceSqToEntity(this.renderManager.livingPlayer);
+                float f = entity.isSneaking() ? NAME_TAG_RANGE_SNEAK : NAME_TAG_RANGE;
+
+                if (d0 < (double)(f * f))
+                {
+                    String s = entity.getDisplayName().getFormattedText();
+                    float f1 = 0.02666667F;
+                    GlStateManager.alphaFunc(516, 0.1F);
+
+                    if (entity.isSneaking())
+                    {
+                        FontRenderer fontrenderer = this.getFontRendererFromRenderManager();
+                        GlStateManager.pushMatrix();
+                        GlStateManager.translate((float)x, (float)y + entity.height + 0.5F - (entity.isChild() ? entity.height / 2.0F : 0.0F), (float)z);
+                        GL11.glNormal3f(0.0F, 1.0F, 0.0F);
+                        GlStateManager.rotate(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+                        GlStateManager.rotate(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+                        GlStateManager.scale(-0.02666667F, -0.02666667F, 0.02666667F);
+                        GlStateManager.translate(0.0F, 9.374999F, 0.0F);
+                        GlStateManager.disableLighting();
+                        GlStateManager.depthMask(false);
+                        GlStateManager.enableBlend();
+                        GlStateManager.disableTexture2D();
+                        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+                        int i = fontrenderer.getStringWidth(s) / 2;
+                        Tessellator tessellator = Tessellator.getInstance();
+                        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+                        worldrenderer.begin(7, DefaultVertexFormats.POSITION_COLOR);
+                        worldrenderer.pos((double)(-i - 1), -1.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+                        worldrenderer.pos((double)(-i - 1), 8.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+                        worldrenderer.pos((double)(i + 1), 8.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+                        worldrenderer.pos((double)(i + 1), -1.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+                        tessellator.draw();
+                        GlStateManager.enableTexture2D();
+                        GlStateManager.depthMask(true);
+                        fontrenderer.drawString(s, -fontrenderer.getStringWidth(s) / 2, 0, 553648127);
+                        GlStateManager.enableLighting();
+                        GlStateManager.disableBlend();
+                        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+                        GlStateManager.popMatrix();
+                    }
+                    else {
+                        this.renderOffsetLivingLabel(entity, x, y - (entity.isChild() ? (double)(entity.height / 2.0F) : 0.0D), z, s, 0.02666667F, d0);
+                    }
+                }
+            }
+
+            if (!Reflector.RenderLivingEvent_Specials_Post_Constructor.exists() || !Reflector.postForgeBusEvent(Reflector.RenderLivingEvent_Specials_Post_Constructor, new Object[] {entity, this, Double.valueOf(x), Double.valueOf(y), Double.valueOf(z)}))
+            {
+                ;
+            }
+        }
+    }
